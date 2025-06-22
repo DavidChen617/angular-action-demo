8d8e5577d7aebec0c3ed1d8f0b2db005d4907b73
DavidChen617 <a880617c@gmail.com>
2025-06-22 15:54:15 +0800

    fix action yaml

--- calculate_hashing_tools/src/md5_algorithm.js
+++ calculate_hashing_tools/src/md5_algorithm.js
@@ -1,6 +1,6 @@
 import { readFile, stat } from "node:fs/promises";
 import { createHash } from "node:crypto";
-
+import path from "node:path";
 export default async function md5File(filePath) {
   const fullPath = path.resolve(filePath);
   const buffer = await readFile(filePath);
